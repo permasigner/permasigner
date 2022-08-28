9a6e3b68ae72bacb605048a0d8da878b4b14103e
superrnovae <vladislavp@pm.me>
2022-08-28 22:55:12 +0200

    previous approach is better

--- permasigner/__main__.py
+++ permasigner/__main__.py
@@ -1,137 +1,5 @@
 import argparse
-import sys
-
 from permasigner import Permasigner
-
-path_options = [{
-    "flags": ["-f", "--folder"],
-    "args": {
-        "type": str,
-        "dest": "folder",
-        "required": False,
-        "help": "the direct path of a directory containing multiple ipas",
-    }
-}, {
-    "flags": ["-u", "--url"],
-    "args": {
-        "type": str,
-        "dest": "url",
-        "required": False,
-        "help": "the direct URL of the IPA"
-    }
-}, {
-    "flags": ["-p", "--path"],
-    "args": {
-        "type": str,
-        "dest": "path",
-        "required": False,
-        "help": "the direct path of the IPA"
-    }
-}]
-
-options = [{
-    "flags": ["-d", "--debug"],
-    "args": {
-        "action": "store_true",
-        "dest": "debug",
-        "required": False,
-        "help": "show debug output, only useful for testing",
-    }
-}, {
-    "flags": ["-c", "--codesign"],
-    "args": {
-        "action": "store_true",
-        "dest": "codesign",
-        "required": False,
-        "help": "use codesign instead of ldid (only for macOS)"
-    }
-}, {
-    "flags": ["-i", "--install"],
-    "args": {
-        "action": "store_true",
-        "dest": "install",
-        "required": False,
-        "help": "install the application to your device"
-    }
-}, {
-    "flags": ["-o", "--output"],
-    "args": {
-        "type": str,
-        "dest": "output",
-        "required": False,
-        "help": "specify output path"
-    }
-}, {
-    "flags": ["-b", "--bundle-id"],
-    "args": {
-        "type": str,
-        "dest": "bundleid",
-        "required": False,
-        "help": "specify new bundle id"
-    }
-}, {
-    "flags": ["-n", "--name"],
-    "args": {
-        "type": str,
-        "dest": "name",
-        "required": False,
-        "help": "specify new app name"
-    }
-}, {
-    "flags": ["-a", "--author"],
-    "args": {
-        "type": str,
-        "dest": "author",
-        "required": False,
-        "help": "specify new app author"
-    }
-}, {
-    "flags": ["-m", "--min-ver"],
-    "args": {
-        "type": str,
-        "dest": "minver",
-        "required": False,
-        "help": "specify new minimum app version"
-    }
-}, {
-    "flags": ["-l", "--ldid-fork"],
-    "args": {
-        "type": str,
-        "dest": "ldidfork",
-        "required": False,
-        "help": "specify a fork of ldid"
-    }
-}, {
-    "flags": ["-e", "--entitlements"],
-    "args": {
-        "type": str,
-        "dest": "entitlements",
-        "required": False,
-        "help": "specify entitlements that will be merged"
-    }
-}, {
-    "flags": ["-t", "--tcprelay"],
-    "args": {
-        "type": str,
-        "dest": "tcprelay",
-        "required": False,
-        "help": "optional args for tcprelay (ex: 22:2222:localhost:/var/run/usbmuxd)"
-    }
-}, {
-    "flags": ["-z", "--no-ldid-check"],
-    "args": {
-        "action": "store_true",
-        "dest": "nocheckldid",
-        "required": False,
-        "help": "disable ldid hash checking"
-    }
-}, {
-    "flags": ["-V", "--version"],
-    "args": {
-        "action": "version",
-        "version": "Permasigner "
-    }
-}]
 
 
 def main(argv=None, in_package=None) -> None:
@@ -141,17 +9,50 @@ def main(argv=None, in_package=None) -> None:
     in_package = False if in_package is None else in_package
 
     parser = argparse.ArgumentParser()
-    path_group = parser.add_mutually_exclusive_group(required=True)
+    input_group = parser.add_mutually_exclusive_group(required=False)
 
-    for option in path_options:
-        path_group.add_argument(*option.get("flags"), **option.get("args"))
+    input_group.add_argument('-f', '--folder', type=str,
+                             help="sign multiple IPAs from a direct path to a folder")
+    input_group.add_argument('-u', '--url', type=str,
+                             help="the direct URL of the IPA to be signed")
+    input_group.add_argument('-p', '--path', type=str,
+                             help="the direct local path of the IPA to be signed")
 
-    for option in options:
-        parser.add_argument(*option.get("flags"), **option.get("args"))
-
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help="shows some debug info, only useful for testing")
+    parser.add_argument('-c', '--codesign', action='store_true',
+                        help="uses codesign instead of ldid")
+    parser.add_argument('-i', '--install', action='store_true',
+                        help="installs the application to your device")
+    parser.add_argument('-o', '--output', type=str,
+                        help="specify output path")
+    parser.add_argument('-b', '--bundleid', type=str,
+                        help="specify new bundle id")
+    parser.add_argument('-n', '--name', type=str,
+                        help="specify new app name")
+    parser.add_argument('-a', '--author', type=str,
+                        help="specify new app author")
+    parser.add_argument('-m', '--minver', type=str,
+                        help="specify new minimum app version (14.0 recommended)")
+    parser.add_argument('-V', '--version', action='store_true',
+                        help='show current version and exit', )
+    parser.add_argument('-l', '--ldidfork', type=str,
+                        help="specify a fork of ldid (eg. ProcursusTeam, itsnebulalol [default])")
+    parser.add_argument('-t', '--tcprelay', type=str,
+                        help="optional args for tcprelay (ex: 22:2222:localhost:/var/run/usbmuxd)")
+    parser.add_argument('-e', '--entitlements', type=str,
+                        help="path to entitlements file")
+    parser.add_argument('-N', '--no-ldid-check', dest='nocheckldid', action='store_true',
+                        help="disable ldid hash checking")
     args = parser.parse_args()
-    permasigner = Permasigner(in_package, args)
-    permasigner.main()
+
+    if args.version:
+        from permasigner import utils
+        print(f"Permasigner v{utils.get_version(in_package)}")
+        exit(0)
+
+    ps = Permasigner(in_package, args)
+    ps.main()
 
 
 if __name__ == "__main__":
