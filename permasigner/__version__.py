95d346206e59c5f82372fb86eaa0b1a49cf5bcfa
superrnovae <vladislavp@pm.me>
2022-09-11 15:42:38 +0200

    Remove prints and spaces

--- permasigner/__version__.py
+++ permasigner/__version__.py
@@ -2,5 +2,5 @@ try:
     import importlib.metadata as importlib_metadata
 except ModuleNotFoundError:
     import importlib_metadata
-print(__package__)
+
 __version__ = importlib_metadata.version(__package__)
