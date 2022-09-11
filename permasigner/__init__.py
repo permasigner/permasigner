986c5639122574db8e4e5252e248bdebb55a4c43
superrnovae <vladislavp@pm.me>
2022-09-11 15:36:02 +0200

    Switch to poetry

--- permasigner/__init__.py
+++ permasigner/__init__.py
@@ -1,1 +1,2 @@
 from .permasigner import Permasigner
+
--- permasigner/__version__.py
+++ permasigner/__version__.py
@@ -1,1 +1,6 @@
-__version__ = "1.1.3"
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+print(__package__)
+__version__ = importlib_metadata.version(__package__)
